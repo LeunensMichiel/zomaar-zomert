b093793e16bef6ff82dccb93de3518f239cb994d
Michiel Leunens <michiel.leunens@gmail.com>
2026-09-20 13:36:06 +0200

    chore: appendix

--- components/footer-tear-color.tsx
+++ components/footer-tear-color.tsx
@@ -5,5 +5,11 @@ import { colorHex, type TearColor } from "./paper-tear";
 export const FOOTER_TEAR_VAR = "--footer-tear";
 
 export function FooterTearColor({ color }: { color: TearColor }) {
-  return <style>{`:root{${FOOTER_TEAR_VAR}:${colorHex[color]}}`}</style>;
+  const selector = `[data-footer-tear="${color}"]:not([hidden] *)`;
+  return (
+    <>
+      <span hidden data-footer-tear={color} />
+      <style>{`:root:has(${selector}){${FOOTER_TEAR_VAR}:${colorHex[color]}}`}</style>
+    </>
+  );
 }
